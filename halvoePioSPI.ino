c283e5b21865e46fec8f47fd2c9c39057935ff9a
SamHalvoe <46531812+SamHalvoe@users.noreply.github.com>
2024-08-04 14:40:31 +0200

    some refactoring

--- halvoePioSPI.ino
+++ halvoePioSPI.ino
@@ -1,9 +1,10 @@
 #include "halvoePioSPI.hpp"
 
-#include <cstring>
+#include <array>
 
 const pin_size_t HALVOE_SPI_PIN_RX = 10;
 const pin_size_t HALVOE_SPI_PIN_SCK = 11;
+const pin_size_t HALVOE_SPI_PIN_CS = 12;
 const size_t RX_FIFO_SIZE_BYTES = 8;
 int stateMachine = -1;
 
@@ -12,16 +13,59 @@ const size_t FRAME_HEIGHT = 240;
 const size_t FRAME_WIDTH = 320;
 const size_t FRAME_SIZE = FRAME_HEIGHT * FRAME_WIDTH;
 
-const size_t dmaBufferSize = FRAME_SIZE;
-uint8_t dmaBuffer[dmaBufferSize];
-bool isDmaFull = false;
+const int16_t PIN_STATUS_NOT_YET_READ = -1;
+int16_t currentPinStatus = PIN_STATUS_NOT_YET_READ;
+int16_t previousPinStatus = PIN_STATUS_NOT_YET_READ;
 
-void handleDmaIrq()
+const size_t dmaBufferSize = 16384; // in bytes (uint8_t)
+const size_t dmaBufferCount = 2;
+using DmaBuffer = std::array<uint8_t, dmaBufferSize>;
+using DmaBufferArray = std::array<DmaBuffer, dmaBufferCount>;
+DmaBufferArray dmaBufferArray;
+size_t currentDmaBufferIndex = 0;
+size_t previousDmaBufferIndex = currentDmaBufferIndex;
+
+DmaBuffer& getCurrentDmaBuffer()
 {
-  isDmaFull = true;
-  clear_dma_interrupt_request();
-  restart_dma_channel(dmaBuffer);
+  return dmaBufferArray[currentDmaBufferIndex];
+}
+
+DmaBuffer& getPreviousDmaBuffer()
+{
+  return dmaBufferArray[previousDmaBufferIndex];
+}
+
+DmaBuffer& getNextDmaBuffer()
+{
+  previousDmaBufferIndex = currentDmaBufferIndex;
+  ++currentDmaBufferIndex;
+  if (currentDmaBufferIndex == dmaBufferArray.size()) { currentDmaBufferIndex = 0; }
+  return dmaBufferArray[currentDmaBufferIndex];
+}
+
+void __time_critical_func(handleDmaIrq)()
+{
+  halvoePioSPI::clear_dma_interrupt_request();
+  //halvoePioSPI::restart_dma_channel(dmaBuffer, dmaBufferSize);
   Serial.println("handleDmaIrq() done");
+}
+
+volatile bool isCommandCompleted = false;
+
+void __time_critical_func(handleCSIrq)()
+{
+  currentPinStatus = digitalReadFast(HALVOE_SPI_PIN_CS) ? HIGH : LOW;
+  Serial.println(currentPinStatus);
+  Serial.println(previousPinStatus);
+  if (currentPinStatus == HIGH && previousPinStatus == LOW)
+  {
+    isCommandCompleted = true;
+    halvoePioSPI::restart_dma_channel(getNextDmaBuffer().data(), dmaBufferSize);
+  }
+
+  previousPinStatus = currentPinStatus;
+  
+  Serial.println("handleCSIrq() done");
 }
 
 void setup()
@@ -30,8 +74,14 @@ void setup()
   while (not Serial) { delay(100); }
   Serial.println("Serial is ready.");
 
-  std::memset(dmaBuffer, 2, dmaBufferSize);
-  stateMachine = setupPioSPI(pio1, HALVOE_SPI_PIN_RX, dmaBuffer, dmaBufferSize, handleDmaIrq);
+  for (auto& dmaBuffer: dmaBufferArray)
+  {
+    dmaBuffer.fill(0);
+  }
+  
+  pinMode(HALVOE_SPI_PIN_CS, INPUT);
+  attachInterrupt(digitalPinToInterrupt(HALVOE_SPI_PIN_CS), handleCSIrq, CHANGE);
+  stateMachine = halvoePioSPI::setup(pio1, HALVOE_SPI_PIN_RX, getCurrentDmaBuffer().data(), dmaBufferSize, handleDmaIrq);
   Serial.println("Leaving setup()");
 
   pinMode(IS_READY_PIN, OUTPUT);
@@ -40,15 +90,25 @@ void setup()
 
 void loop()
 {
-  // nothing to do here
-  if (isDmaFull)
+  if (isCommandCompleted)
   {
     String printString;
 
-    for (size_t index = 0; index < dmaBufferSize; ++index)
+    for (size_t index = 0; index < 128/*dmaBufferSize*/; ++index)
     {
-      if (dmaBuffer[index] < 10) { printString.concat('0'); }
-      printString.concat(String(dmaBuffer[index], HEX));
+      if (getCurrentDmaBuffer()[index] < 10) { printString.concat('_'); }
+      printString.concat(String(getCurrentDmaBuffer()[index], HEX));
+
+      if ((index + 1) % 64 == 0)
+      {
+        printString.concat('\n');
+      }
+    }
+
+    for (size_t index = 0; index < 128/*dmaBufferSize*/; ++index)
+    {
+      if (getPreviousDmaBuffer()[index] < 10) { printString.concat('_'); }
+      printString.concat(String(getPreviousDmaBuffer()[index], HEX));
 
       if ((index + 1) % 64 == 0)
       {
@@ -58,6 +118,6 @@ void loop()
 
     Serial.println(printString);
 
-    isDmaFull = false;
+    isCommandCompleted = false;
   }
 }
